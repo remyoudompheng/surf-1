8758be4f403c713581eb78678dc4535e0f56abd7
Rémy Oudompheng <oudomphe@math.unice.fr>
2010-01-17 12:59:56 +0100

    Build fixes.

--- autogen.sh
+++ autogen.sh
@@ -5,20 +5,12 @@ if ! test -x autogen.sh; then
     exit 10;
 fi;
 
-echo "creating yaccsrc/addDefaultSymbols.cc from defaults.pic"
-perl tools/convertScript.pl defaults.pic >yaccsrc/addDefaultSymbols.cc
-if test -x configure; then
-    echo "configure exists...nothing to do ???"
-    exit 1;
-fi;
+# echo "creating yaccsrc/addDefaultSymbols.cc from defaults.pic"
+# perl tools/convertScript.pl defaults.pic >yaccsrc/addDefaultSymbols.cc
 
 automake -a --foreign
 aclocal
 autoconf
 
-
 echo "running ./configure $*"
 ./configure $*
-cd yaccsrc
-make polyyacc.c
-make polylex.c
